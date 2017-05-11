91a6e2e1c1d92e64dd8539b3d5a5b5402ac76827
Harry <hfitz83@gmail.com>
2017-05-11 18:36:06 +0100

    attempt to fix 404 page. need to redir rather than show a page which then redirects.

--- app.js
+++ app.js
@@ -34,11 +34,11 @@ app.use((req, res, next) => {
 // error handler
 app.use((err, req, res) => {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
+  // res.locals.message = err.message;
+  // res.locals.error = req.app.get('env') === 'development' ? err : {};
+  //
+  // // render the error page
+  // res.status(err.status || 500);
   // res.location('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
   res.render('404', {
     title: err.status,
